117f7ebb2a30b64718b0765a3307b06862d45a25
Luc Comeau <comeaul@byui.edu>
2021-02-11 17:28:57 -0700

    week 06 team 2 files

---
+++ week06/team2/teach2.py
@@ -0,0 +1,38 @@
+"""
+Course: CSE353
+File: teach2.py (week 06)
+"""
+
+import numpy as np
+import cv2
+
+# ===========================================================
+def task1():
+    """
+    - Using a live video (or person video or the Internet)
+    - Apply a mask to the video
+    - Apply a filter to each frame
+        - sharpen it
+        - smooth it
+        - Try sobel kernals
+        - try laplacian opencv function
+    """
+
+# ===========================================================
+def task2():
+    """
+    Compare the candle images (candle*.png) to find which two are closest
+    Questions:
+        - How are you going to compare them?
+        - Would smoothing the images help in this case?
+    """
+
+
+# ===========================================================
+def main():
+    task1()
+    task2()
+
+
+if __name__ == '__main__':
+	main()
