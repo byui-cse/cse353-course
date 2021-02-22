583e5c908c1a03cd317809ad81a86e63b0632fb1
Luc Comeau <comeaul@byui.edu>
2021-02-21 23:11:39 -0700

    week 08

---
+++ week08/assignment/assignment.py
@@ -0,0 +1,24 @@
+"""
+Course: CSE 353
+Lesson Week: 08
+File: assingment.py
+Author: <Your name>
+
+Instructions:
+
+- Look for directions in I-Learn
+
+"""
+
+import numpy as np
+import cv2
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
