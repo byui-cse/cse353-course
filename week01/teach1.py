0fd9c0b8bf8556e7c8bce448f7735d68444b30d5
Luc Comeau <comeaul@byui.edu>
2021-04-19 17:13:53 -0600

    week 01 team activities

--- week01/teach1.py
+++ week01/teach1.py
@@ -2,184 +2,95 @@
 Course: CS312
 Week: 01
 File: teach1.py
-Description:
+Author: Brother Comeau
+Description: Simple shopping list program
 
-   Simple Python problems
+Instructions:
 
-   You are NOT submitting this program.
+Write a program that allows a user to create and use a shopping list.  Your
+program will display a menu of options to the user.  The user is free
+to select any option from the menu in any order.
+
+Extra Features once you are finished with the requirements below:
+1) Allow the user to enter the number of each item they want to purchase.
+2) Add a "checked" feature for each item. This will be used to indicate 
+   if the item is checked off the list without removing it.
+
+
+Sample Output:
+
+Welcome to the shopping list program.
+
+Menu
+   n - New shopping list item
+   d - Display shopping list
+   e - Edit an item in the list
+   c - Check or remove item from list
+   ? - Display this menu
+   q - Quit program
+> n
+Enter shopping item: Bread
+> n
+Enter shopping item: Milk
+> n
+Enter shopping item: Eggs
+> d
+********* Shopping list *********
+* 1  Bread                      *
+* 2  Milk                       *
+* 3  Eggs                       *
+*********************************
+> e
+Which item do you want to change?: 2
+Enter your change: Whole Milk
+> d
+********* Shopping list *********
+* 1  Bread                      *
+* 2  Whole Milk                 *
+* 3  Eggs                       *
+*********************************
+> e
+Which item do you want to change?: 10
+Error: There is no item at that position
+> e
+Which item do you want to change?: 0
+Error: There is no item at that position
+> ?
+Menu
+   n - New shopping list item
+   d - Display shopping list
+   e - Edit an item in the list
+   c - Check or remove item from list
+   ? - Display this menu
+   q - Quit program
+> c
+Which item do you want to check?: 10
+Error: There is no item at that position
+> c
+Which item do you want to check?: 1
+Item 1 was checked and removed
+> d
+********* Shopping list *********
+* 1  Whole Milk                 *
+* 2  Eggs                       *
+*********************************
+> n
+Enter shopping item: Ice Cream
+> d
+********* Shopping list *********
+* 1  Whole Milk                 *
+* 2  Eggs                       *
+* 3  Ice Cream                  *
+*********************************
+> q
+Good bye
+
 """
 
-# ---------------------------------------------------------------------------------​
-# Variables
-
-# Problem: display "Hello World" on the screen
-
-
-# Problem: Create two variables and add them together. Print the results.
-
-
-# Problem: What is the 123 to the power of 5?
-
-
-# Problem: What does this code display? Why?
-# x = 110
-# if (1 < x < 100):
-#     print('true')
-# else:
-#     print('false')
-
-
-# Problem: Given x = 9 and y = 99, write code to swap the values of these variables so x = 99 and y = 9
-
-x = 9
-y = 99
-
-# Swap x and y
-print(x, y)
-
-
-# Problem: Give me an example of the python equivalence of C's '?: ' ternary operator
-# int x = 9;
-# int y = (x > 5) ? 1: 2;
-
-
-
-# ---------------------------------------------------------------------------------​
-# Loops
-
-# Problem: Display numbers 1 to 10 using a loop
-
-
-# Problem: Display numbers 1 to 10 seperated by commas using a loop
-# 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-
-
-
-# Problem: Display the numbers that are multiples of 3 from 0 to 19 using a loop
-
-
-
-# Problem: Display a multiplication table 5 x 5 using loops
-
-
-# Problem: Using loop(s) and only a '*' or blank one at a time, display the following
-# Make the code handle any size of diamond (ie., this diamond below is size = 5)
-#
-#      *
-#     * *
-#    *   *
-#   *     *
-#  *       *
-#   *     *
-#    *   *
-#     * *
-#      *
-
-
-# ---------------------------------------------------------------------------------​
-# Strings and Formating
-# Problem: Create two strings, "add" them together and then display the results
-
-
-# Problem: Given the string "ABCDE", display the 3rd character
-
-
-# Problem: Write code to calculate the length of the string "1234567890"
-
-
-# Problem: Given the string "The red house is on the hill", parse each word out to display
-# The
-# red
-# house
-# is
-# on
-# the
-# hill
-
-
-# Problem: Use the python f-strings to display the following. You can only use the variable x.
-# The numbers 1 + 1 equal 2
-# x = 1
-
-
-# Problem: Use the python print()/f-strings to display the float number following value as 3.14
-# pi = 3.141592653589793
-
-
-# Problem: Display the length of the string words.
-# words = 'The red house is on the hill'
-
-
-# ---------------------------------------------------------------------------------​
-# Functions
-
-
-# Problem: Complete this function that will take a value and return (value + 1)
-
-# Write function here
-def addOne(x):
-    pass
-
-print(addOne(5))
-
-
-# Problem: Complete this factoral function using recursion
-def factoral(x):
-    pass
-
-print('5! =', factoral(5))
-
-
-# Problem: Complete this function so it can "return" the values 100 and 999.
-def returnTwo():
+def main():
     pass
 
 
-# Problem: Add code to call the returnTwo() function and handle two return values
-
-
-# ---------------------------------------------------------------------------------​
-# Lists
-# Problem: Create a list of numbers 1 to 10
-
-
-# Problem: Using the list created above, display the 3rd value in the list
-
-
-# Problem: Create two lists and combine them together
-
-
-# Problem: Create a list of ten values called "values". Create another list based on the first 5 values in the list "values".
-
-
-# Problem: Create a list of even numbers from 1 to 100 in one statement.
-
-
-# Problem: With the above list of even numbers, count how many are in the list.
-
-
-# Problem: Create a tuple of values 1, 2 and 3
-
-
-# Problem: Using the above tuple, assign the variables a, b and c to be the values in the tuple.
-
-
-# Problem: Create a list of 10 values from 1 to 10. Create a function that will take that list as an argument. Have that function multiple each value by 3 and return the new list.
-# Original list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# New list: [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
-
-def multBy3():
-    pass
-
-
-
-# ---------------------------------------------------------------------------------​
-# Dictionaries
-# Problem: Python dictionaries are like the C++ map container.
-
-
-# Problem: Create a dictionary where the integer 1 is mapped to "one", 2 => "two" and 3 => "three". Also, write a print statement to display the full dictionary.
-
-
-# Problem: Using the above dictionary, write a print statement to display what 2 is mapped to.
-
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
--- week01/teach2.py
+++ week01/teach2.py
@@ -1,100 +1,137 @@
 """
-Course: CS312
-Week: 01
+Course: CSE 353
+Lesson Week: 01
 File: teach2.py
-Description:
-   Simple shopping list program
-
-   You are NOT submitting this program.
+Author: Brother Comeau
+Problems taken from: https://edabit.com/challenges/python3
 """
 
+""" 
+A number is said to be Disarium if the sum of its digits raised to their 
+respective positions is the number itself.
+
+Create a function that determines whether a number is a Disarium or not.
+
+Examples
+disarium(75) ➞ False   # 7^1 + 5^2 = 7 + 25 = 32
+disarium(135) ➞ True   # 1^1 + 3^2 + 5^3 = 1 + 9 + 125 = 135
+disarium(544) ➞ False
+disarium(518) ➞ True
+disarium(466) ➞ False
+disarium(8) ➞ True
 """
-Instructions:
-
-Write a program that allows a user to create and use a shopping list.  Your
-program will display a menu of options to the user.  The user is free
-to select any option from the menu in any order.
-
-Extra Features once you are finished with the requirements below:
-1) Allow the user to enter the number of each item they want to purchase.
-2) Add a "checked" feature for each item. This will be used to indicate 
-   if the item is checked off the list without removing it.
-
-
-Sample Output:
-
-Welcome to the shopping list program.
-
-Menu
-   n - New shopping list item
-   d - Display shopping list
-   e - Edit an item in the list
-   c - Check or remove item from list
-   ? - Display this menu
-   q - Quit program
-> n
-Enter shopping item: Bread
-> n
-Enter shopping item: Milk
-> n
-Enter shopping item: Eggs
-> d
-********* Shopping list *********
-* 1  Bread                      *
-* 2  Milk                       *
-* 3  Eggs                       *
-*********************************
-> e
-Which item do you want to change?: 2
-Enter your change: Whole Milk
-> d
-********* Shopping list *********
-* 1  Bread                      *
-* 2  Whole Milk                 *
-* 3  Eggs                       *
-*********************************
-> e
-Which item do you want to change?: 10
-Error: There is no item at that position
-> e
-Which item do you want to change?: 0
-Error: There is no item at that position
-> ?
-Menu
-   n - New shopping list item
-   d - Display shopping list
-   e - Edit an item in the list
-   c - Check or remove item from list
-   ? - Display this menu
-   q - Quit program
-> c
-Which item do you want to check?: 10
-Error: There is no item at that position
-> c
-Which item do you want to check?: 1
-Item 1 was checked and removed
-> d
-********* Shopping list *********
-* 1  Whole Milk                 *
-* 2  Eggs                       *
-*********************************
-> n
-Enter shopping item: Ice Cream
-> d
-********* Shopping list *********
-* 1  Whole Milk                 *
-* 2  Eggs                       *
-* 3  Ice Cream                  *
-*********************************
-> q
-Good bye
-
-"""
-
-def main():
+def disarium(value):
     pass
 
 
-if __name__ == "__main__":
-    # execute only if run as a script
+"""
+Valid Hex Code
+Create a function that determines whether a string is a valid hex code.
+
+A hex code must begin with a pound key # and is exactly 6 characters 
+in length. Each character must be a digit from 0-9 or an alphabetic 
+character from A-F. All alphabetic characters may be uppercase or lowercase.
+
+Examples
+is_valid_hex_code("#CD5C5C") ➞ True
+is_valid_hex_code("#EAECEE") ➞ True
+is_valid_hex_code("#eaecee") ➞ True
+is_valid_hex_code("#CD5C58C") ➞ False   # Length exceeds 6
+is_valid_hex_code("#CD5C5Z") ➞ False    # Not all alphabetic characters in A-F
+is_valid_hex_code("#CD5C&C") ➞ False    # Contains unacceptable character
+is_valid_hex_code("CD5C5C") ➞ False     # Missing #
+"""
+def is_valid_hex_code(code):
+    pass
+
+
+"""
+Valid matching ()
+
+Create a function to takes a given string and returns True if the () match
+
+matching_brackets('()')  -> True
+matching_brackets('(3 + 8)')  -> True
+matching_brackets('(()())')  -> True
+matching_brackets('(cat((house)()))')  -> True
+matching_brackets('((())))(')  -> False
+matching_brackets('())')  -> False
+"""
+
+def matching_brackets(expression):
+    pass
+
+"""
+LCM of More Than Three Numbers
+Create a function that takes a list of more than three numbers and returns
+the Least Common Multiple (LCM).  The LCM of a list of numbers is the 
+smallest positive number that is divisible by each of the numbers in the list.
+
+Examples
+get_lcm([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) ➞ 2520
+get_lcm([13, 6, 17, 18, 19, 20, 37]) ➞ 27965340
+get_lcm([44, 64, 12, 17, 65]) ➞ 2333760
+"""
+def get_lcm(values):
+    pass
+
+
+"""
+Letter Shifting
+Create a function that takes a string and shifts the letters to the right by an amount n but not the whitespace.
+
+Examples
+shift("Boom", 2) ➞ "omBo"
+
+shift("This is a test",  4) ➞ "test Th i sisa"
+
+shift("A B C D E F G H", 5) ➞  "D E F G H A B C"
+Notes
+Keep the case as it is.
+n can be larger than the total number of letters.
+"""
+def shift(sentence, amount):
+    pass
+
+
+
+def main():
+
+    # TODO - comment out the function that you are testing
+
+    # assert(disarium(544) == False)
+    # assert(disarium(518) == True)
+    # assert(disarium(466) == False)
+    # assert(disarium(8) == True)
+
+
+    # assert(is_valid_hex_code("#CD5C5C") == True)
+    # assert(is_valid_hex_code("#EAECEE") == True)
+    # assert(is_valid_hex_code("#eaecee") == True)
+    # assert(is_valid_hex_code("#CD5C58C") == False)
+    # assert(is_valid_hex_code("#CD5C5Z") == False)
+    # assert(is_valid_hex_code("#CD5C&C") == False)
+    # assert(is_valid_hex_code("CD5C5C") == False)
+
+
+    # assert(matching_brackets('()') == True)
+    # assert(matching_brackets('(3 + 8)') == True)
+    # assert(matching_brackets('(()())') == True)
+    # assert(matching_brackets('(cat((house)()))') == True)
+    # assert(matching_brackets('((())))(') == False)
+    # assert(matching_brackets(')(') == False)
+
+
+    # assert(get_lcm([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 2520)
+    # assert(get_lcm([13, 6, 17, 18, 19, 20, 37]) == 27965340)
+    # assert(get_lcm([44, 64, 12, 17, 65]) == 2333760)
+
+
+    # assert(shift("Boom", 2) == 'omBo')
+    # assert(shift("A B C D E F G H", 5) == 'D E F G H A B C')
+
+    pass
+
+
+if __name__ == '__main__':
     main()
